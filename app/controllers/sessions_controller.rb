cc179bacd3ca9f5da50aca894a8e04c64034b1ee
pennyyeung <penny.yeung@advitor.com>
2012-10-16 00:12:45 +0800

    haha
    ahaha

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+	def new
+		
+	end
+
+	def create
+		flash.now[:error] = "Invalid email/password combination"
+		render 'new'
+	end
+
+	def destroy
+		
+	end
+
+end
