ac9392adf298448369f12f1729c5d5b2ddffb706
pennyyeung <penny.yeung@advitor.com>
2012-11-03 19:01:48 +0800

    Init

---
+++ test/integration/com/grailsinaction/UserIntegrationTests.groovy
@@ -0,0 +1,22 @@
+package com.grailsinaction
+import grails.test.*
+import static org.junit.Assert.*
+import org.junit.*
+
+class UserIntegrationTests  {
+
+    @Before
+    void setUp() {
+        // Setup logic here
+    }
+
+    @After
+    void tearDown() {
+        // Tear down logic here
+    }
+
+    @Test
+    void testSomething() {
+        fail "Implement me"
+    }
+}
